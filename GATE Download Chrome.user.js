1cc8283e371f435b95be122dbe685b0d448e7e4a
Sagar Pawar <curiosobesta@gmail.com>
2017-10-08 21:09:59 +0530

    Add files via upload
    Files compatible with crome

---
+++ GATE Download Chrome.user.js
@@ -0,0 +1,94 @@
+// ==UserScript==
+// @name        GATE Download
+// @namespace   sgrpwr
+// @description Download GATE Lectures
+// @include     http://DUMMYeclassesbyravindra.com/mod/page/view.php?*
+// @include     *://*.vimeo.com/video/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+/*function afterLoad(){
+	console.log("Hurray!! Frame loaded");
+}
+
+var frame_path = "/html/body/div[2]/div/div/div/section/div/div[1]/div/p/iframe"
+var script_path = "/html/body/script[4]"
+
+console.log("Starting UserScript");
+
+var iframe = document.evaluate( frame_path ,document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null ).singleNodeValue;
+console.log("Searching Done")
+if (iframe != null) {
+  //var link = element.getAttribute("src");
+  var text = iframe.contentDocument.body.innerHTML;
+  console.log("Found Link: "+text);
+
+  iframe.setAttribute("onload", "afterLoad()");
+
+  var iFrameBody= iframe.contentDocument || iframe.contentWindow.document;
+  content= iFrameBody.getElementsByTagName('body')[0].innerHTML;
+  console.log("Content: "+content);
+}
+else{
+	alert("Iframe not found");
+}
+*/
+if (window.top == window.self)  //don't run on top window
+    return;
+
+var markup = document.documentElement.innerHTML;
+console.log('running on a frame');
+//console.log(markup);
+
+script_path = "/html/body/script[2]";
+var script = document.evaluate( script_path ,document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null ).singleNodeValue;
+script = script.innerHTML;
+console.log('Here');
+script = script.replace("(function(e,a){", "");
+
+
+//Find JSON
+var json = "";
+var stack = 0;
+var first = true;
+var i;
+for(i=0; i<script.length && (first || stack!==0); i++){
+		var c = script.charAt(i);
+		if(c == '{'){
+			first = false;
+			stack++;
+		}
+		else if(c == '}'){
+			stack--;
+		}
+
+		if(stack===0 && first)
+			continue;
+		json += c;
+
+}
+
+//Parse JSON
+var url = "Cannot Find";
+var main = JSON.parse(json);
+console.log("==> "+main.request.files.progressive);
+var progressive = main.request.files.progressive;
+console.log("==> Looking for 360p Quality...");
+for (i in progressive){
+	progress = progressive[i];
+	if(progress.quality == "360p"){
+		console.log("Hurray!! Link Found");
+		url = progress.url;
+		break;
+	}
+}
+console.log("==> URL: "+url);
+
+
+//Call parent function
+var domain = "http://eclassesbyravindra.com";
+var message = url;
+console.log("Sending Message to parent...");
+window.top.postMessage(message, '*');
+console.log("Message Sent to parent");
---
+++ Vimeo Download Button Chrome.user.js
@@ -0,0 +1,64 @@
+// ==UserScript==
+// @name        Vimeo Download Button
+// @namespace   sgrpwr
+// @description Download GATE Lectures
+// @include     http://eclassesbyravindra.com/mod/page/view.php?*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+console.log("Parent Executing...");
+
+var url="http://google.com/";
+//alert(call("Hi Saggar"));
+
+var body = document.body;
+var input = document.createElement("input");
+
+input.setAttribute("type", "button");
+input.setAttribute("Nothing", "Nothing");
+//body.appendChild(input);
+
+//Add Button
+
+
+//Add Java Script
+var script = document.createElement("script");
+var listner_code = "";
+
+var func_code = "function onUrlFound(url){ alert(\"Hello Sagar\"); }";
+var script_text = document.createTextNode(listner_code);
+script.appendChild(script_text);
+var head = document.head;
+head.appendChild(script);
+
+console.log(head);
+console.log(script);
+console.log(listner_code);
+
+
+console.log("Parent Execution Ended...");
+
+var org_url = "Not known";
+window.addEventListener('message', function(event){
+  console.log("Event Received: "+event.data);
+  if(event.data == "{\"event\":\"ready\"}") return;
+
+  var link = event.data;
+  org_url = link;
+  createButton(link, "Saar");
+});
+
+
+function createButton(url, name){
+  console.log("Creating Button...");
+  var a = document.createElement("a");
+  a.setAttribute("href", url);
+  a.setAttribute("download", "Sagar File.mp4");
+
+  var btn_text = document.createTextNode("Download");
+  a.appendChild(btn_text);
+
+  body.insertBefore(a, body.childNodes[0]);
+  console.log("Download Button Ready...");
+}
